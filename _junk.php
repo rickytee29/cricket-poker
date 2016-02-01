bd603b88f2a4149603ba8a5d74b2f39337800420
Richard Teemal <richard.teemal@gmail.com>
2016-02-01 18:41:40 -0400

    make it better 4

--- _junk.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-require "../_private/php/tgcm.php";
-$teemalGames =  new Main();
-?>
-
--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Replace XXX with your twitter application variables.
-define('CONSUMER_KEY', 'SPirGIdnIGJLZKci7051yflv5');
-define('CONSUMER_SECRET', 'NUFEZ4ugDLyVZIsXSnJ4ymKDmYb80M24HNxkNNIVhJlfCeUFpj');
-define('OAUTH_CALLBACK', 'http://cricketpoker.teemalgames.com/process.php');
-?>
